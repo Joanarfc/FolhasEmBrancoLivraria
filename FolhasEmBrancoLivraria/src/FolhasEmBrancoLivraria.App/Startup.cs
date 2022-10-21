57a5848444129064f9baa7b33d0253b761c3e18e
Joanarfc <joanarfcarvalho@hotmail.com>
2022-10-21 10:02:54 -0300

    Resolução da injeção de dependência do contexto e dos repositórios na Startup

--- FolhasEmBrancoLivraria/src/FolhasEmBrancoLivraria.App/Startup.cs
+++ FolhasEmBrancoLivraria/src/FolhasEmBrancoLivraria.App/Startup.cs
@@ -1,18 +1,14 @@
 using FolhasEmBrancoLivraria.App.Data;
+using FolhasEmBrancoLivraria.Business.Interfaces;
 using FolhasEmBrancoLivraria.Data.Context;
+using FolhasEmBrancoLivraria.Data.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FolhasEmBrancoLivraria.App
 {
@@ -25,7 +21,6 @@ namespace FolhasEmBrancoLivraria.App
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -41,9 +36,12 @@ namespace FolhasEmBrancoLivraria.App
 
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddScoped<DataDbContext>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -54,7 +52,6 @@ namespace FolhasEmBrancoLivraria.App
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
