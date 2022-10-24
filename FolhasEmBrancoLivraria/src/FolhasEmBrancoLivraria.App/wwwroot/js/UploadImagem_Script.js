a812facc71b1f0ca19ee3b7100ea9ebc8fa2f682
Joanarfc <joanarfcarvalho@hotmail.com>
2022-10-24 17:32:23 -0300

    Customização da View de criação do Produto

---
+++ FolhasEmBrancoLivraria/src/FolhasEmBrancoLivraria.App/wwwroot/js/UploadImagem_Script.js
@@ -0,0 +1,7 @@
+﻿    $("#ImagemUpload").change(function () {
+        $("#img_name").text(this.files[0].name);
+    $("#img_name")[0].style.display = 'block';
+        });
+
+    $("#ImagemUpload").attr("data-val", "true");
+    $("#ImagemUpload").attr("data-val-required", "Fill in the Image field.");
