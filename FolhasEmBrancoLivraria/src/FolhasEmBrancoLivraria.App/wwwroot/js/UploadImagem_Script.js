c7134bd681accc31888d8f932116578080b7a981
Joanarfc <joanarfcarvalho@hotmail.com>
2022-10-25 10:06:32 -0300

    Customização da View de Edição de Produtos

--- FolhasEmBrancoLivraria/src/FolhasEmBrancoLivraria.App/wwwroot/js/UploadImagem_Script.js
+++ FolhasEmBrancoLivraria/src/FolhasEmBrancoLivraria.App/wwwroot/js/UploadImagem_Script.js
@@ -1,7 +1,4 @@
-﻿    $("#ImagemUpload").change(function () {
-        $("#img_name").text(this.files[0].name);
+﻿$("#ImagemUpload").change(function () {
+    $("#img_name").text(this.files[0].name);
     $("#img_name")[0].style.display = 'block';
-        });
-
-    $("#ImagemUpload").attr("data-val", "true");
-    $("#ImagemUpload").attr("data-val-required", "Fill in the Image field.");
+});
---
+++ FolhasEmBrancoLivraria/src/FolhasEmBrancoLivraria.App/wwwroot/js/Validacao_UploadImagem_Script.js
@@ -0,0 +1,2 @@
+﻿$("#ImagemUpload").attr("data-val", "true");
+$("#ImagemUpload").attr("data-val-required", "Preencha o campo da imagem.");
