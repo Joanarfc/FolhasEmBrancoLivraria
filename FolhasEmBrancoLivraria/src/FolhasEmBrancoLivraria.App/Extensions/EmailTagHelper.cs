51e15b657aa6cee654a8d43a5bff4d3b65fdef81
Joanarfc <joanarfcarvalho@hotmail.com>
2022-10-26 15:23:55 -0300

    Customização de uma TagHelper para envio de email

---
+++ FolhasEmBrancoLivraria/src/FolhasEmBrancoLivraria.App/Extensions/EmailTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace FolhasEmBrancoLivraria.App.Extensions
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string EmailDomain { get; set; } = "folhasembranco.com";
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            var emailTo = content.GetContent() + "@" + EmailDomain;
+            output.Attributes.SetAttribute("href", "mailto:" + emailTo);
+            output.Content.SetContent("Envie-nos um email");
+        }
+    }
+}
