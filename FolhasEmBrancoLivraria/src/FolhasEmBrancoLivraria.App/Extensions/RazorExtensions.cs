16a230f13da1ea160876f6810f3ba68b20d49e46
Joanarfc <joanarfcarvalho@hotmail.com>
2022-10-25 21:28:35 -0300

    Customização das Views de Fornecedores com formatação do CPF/CNPJ

---
+++ FolhasEmBrancoLivraria/src/FolhasEmBrancoLivraria.App/Extensions/RazorExtensions.cs
@@ -0,0 +1,14 @@
+﻿using FolhasEmBrancoLivraria.Business.Models;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+
+namespace FolhasEmBrancoLivraria.App.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string CpfCnpjFormatacao(this RazorPage page, TipoFornecedor tipoFornecedor, string documento)
+        {
+            return ((int)tipoFornecedor) == 1 ? Convert.ToInt64(documento).ToString(@"000\.000\.000\-00") : Convert.ToInt64(documento).ToString(@"00\.000\.000\/0000\-00");
+        }
+    }
+}
--- FolhasEmBrancoLivraria/src/FolhasEmBrancoLivraria.Business/Models/TipoFornecedor.cs
+++ FolhasEmBrancoLivraria/src/FolhasEmBrancoLivraria.Business/Models/TipoFornecedor.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
+using System.Xml.Linq;
 
 namespace FolhasEmBrancoLivraria.Business.Models
 {
     public enum TipoFornecedor
     {
+        [Display(Name = "Pessoa Física")]
         PessoaFisica = 1,
+        [Display(Name = "Pessoa Jurídica")]
         PessoaJuridica
     }
 }
